aed54305f0e6e9e93e62f578f42b10bbb27a6bb2
nae_stormrage <stas.markus.2004@mail.ru>
2023-02-13 17:59:50 +0300

    Initial commit

--- scripts/Lab_1.py
+++ scripts/Lab_1.py
@@ -1,6 +1,13 @@
 G_1 = 7
 Zvcw = 4
 a = 8
+
 print("G_1 = ", G_1, ", type(G_1) = ", type(G_1))
 print("Zvcw = ", Zvcw, ", type(Zvcw) = ", type(Zvcw))
 print("a = ", a, ", type(a) = ", type(a))
+
+A0 = ((35/G_1) * Zvcw + G_1 * a - ((a + Zvcw)/a))
+
+print("A0 = ", A0)
+print("Type of A0: ", type(A0))
+
